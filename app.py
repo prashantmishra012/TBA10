9f5056a0914fb13c0b10f7ecdb7427c072db80e7
prashantmishra012 <mishraprashantpm2005@gmail.com>
2025-12-31 07:11:03 +0530

    Fix Streamlit HTML rendering by using components.html instead of st.markdown

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Titu Bhaiya & Soni Didi 10th Anniversary 💖", layout="wide", initial_sidebar_state="collapsed")
 
@@ -15,4 +16,4 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 with open("index.html", "r", encoding="utf-8") as f:
     html = f.read()
 
-st.markdown(html, unsafe_allow_html=True)
+components.html(html, height=800, scrolling=True)
